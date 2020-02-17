c830f5854232fca7f32e00b76f11a30f080b9fbe
github username <github email address>
2020-02-17 12:11:13 -0500

    Added index, show, create, and update CRUD methods.

---
+++ app/controllers/api/v1/medications_controller.rb
@@ -0,0 +1,32 @@
+class Api::V1::MedicationsController < ApplicationController
+
+    def index
+        @medications = Medications.all 
+
+        render json: @medications, status: 200
+    end
+
+    def show
+        @medication = Medication.find(params[:id])
+
+        render json: @medication, status: 200
+    end
+
+    def create
+        @medication = Medication.create(medication_params)
+
+        render json: @medication, status: 200
+    end
+
+    def update
+        @medication = Medication.find(params[:id])
+        @medication.update(medication_params)
+        
+        render json: @medication, status: 200
+    end
+
+    private
+    def medication_params
+        params.require(:note).permit(:name, :class, :indication, :dose, :frequency, :note)
+    end
+end
