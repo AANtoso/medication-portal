e389dc2eab77ec14acd67f6a6a1a343e2cc954da
github username <github email address>
2020-02-17 18:30:15 -0500

    Added prescription attributes.

--- backend-medication-portal/db/migrate/20200217231624_create_prescriptions.rb
+++ backend-medication-portal/db/migrate/20200217231624_create_prescriptions.rb
@@ -1,7 +1,9 @@
 class CreatePrescriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :prescriptions do |t|
-
+      t.integer :prescrition_id
+      t.string :medication_name
+      t.datetime :prescribed_date
       t.timestamps
     end
   end
