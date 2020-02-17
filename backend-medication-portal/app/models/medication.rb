85e6d89eb2b5f8d4f10e6693659e5fb8bfadcf24
github username <github email address>
2020-02-17 18:09:37 -0500

    Added relationships and validations to medication model.

--- backend-medication-portal/app/models/medication.rb
+++ backend-medication-portal/app/models/medication.rb
@@ -1,2 +1,7 @@
 class Medication < ApplicationRecord
+    has_many :presctiptions, dependent: :destroy
+    has many :patients, through: :presctiptions
+
+    validates :name, presence: true
+    validates :name, uniqueness: { case_sensitive: false}
 end
