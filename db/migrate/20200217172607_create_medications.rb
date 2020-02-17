12b86f0402df66f0b963f280cdae0916e0804021
github username <github email address>
2020-02-17 12:49:23 -0500

    Added medication attributes.

--- db/migrate/20200217172607_create_medications.rb
+++ db/migrate/20200217172607_create_medications.rb
@@ -1,7 +1,12 @@
 class CreateMedications < ActiveRecord::Migration[5.2]
   def change
     create_table :medications do |t|
-      
+      t.string :name 
+      t.string :class
+      t.string :indication
+      t.string :dose
+      t.string :frequency
+      t.text :note
       t.timestamps
     end
   end
