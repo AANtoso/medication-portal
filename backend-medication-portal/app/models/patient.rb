ff132faa97f49bb3f0e0bf277bf835d4ecc60009
AANtoso <adjoaan@gmail.com>
2020-02-23 20:01:08 -0500

    Corrected relationshio, created patient serializer.

--- backend-medication-portal/app/models/patient.rb
+++ backend-medication-portal/app/models/patient.rb
@@ -1,6 +1,6 @@
 class Patient < ApplicationRecord
     has_many :prescriptions, dependent: :destroy
-    has many :medications, through: :prescriptions
+    has_many :medications, through: :prescriptions
 
     validates :name, presence: true
     validates :name, uniqueness: { case_sensitive: false}
--- backend-medication-portal/app/serializers/patient_serializer.rb
+++ backend-medication-portal/app/serializers/patient_serializer.rb
@@ -1,4 +1,5 @@
 class PatientSerializer
   include FastJsonapi::ObjectSerializer
-  attributes 
+  attributes :id, :name, :mrn 
+  has_many :medications, through: :prescriptions
 end
