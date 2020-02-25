5d44453ac331847d7bef5b806ba4d1d504c0ac3a
AANtoso <adjoaan@gmail.com>
2020-02-25 11:01:05 -0500

    Added async.

--- frontend-medication-portal/src/adapters/MedicationsAdapter.js
+++ frontend-medication-portal/src/adapters/MedicationsAdapter.js
@@ -1,10 +1,21 @@
 class MedicationsAdapter {
     constructor() {
-        this.baseUrl = 'http://localhost:3000/api/v1//medications'
+        this.baseUrl = 'http://localhost:3000/api/v1/medications'
     }
 
     getMedications() {
         return fetch(this.baseUrl)
         .then(res => res.json())
+    }
+
+    async createMedicationDB(params) {
+        let medicationData = {
+            name: params[0]
+            class: params[1]
+            indication: params[2]
+            dose: params[3]
+            frequency: params[4]
+            notes: params[5]
+        }
     }
 }
