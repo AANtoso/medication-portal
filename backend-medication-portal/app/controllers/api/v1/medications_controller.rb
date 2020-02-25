e213ca474cfb27cd7afdadc53035b5d086f03be9
AANtoso <adjoaan@gmail.com>
2020-02-25 10:45:21 -0500

    Removes instances and incorporated serializers.

--- backend-medication-portal/app/controllers/api/v1/medications_controller.rb
+++ backend-medication-portal/app/controllers/api/v1/medications_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::MedicationsController < ApplicationController
 
     def index
-        @medications = Medication.all 
+        medications = Medication.all 
 
-        render json: @medications, status: 200
+        render json: medications.to_json(include: [:patients])
     end
 
     def show
-        @medication = Medication.find(params[:id])
-
-        render json: @medication, status: 200
+        medication = Medication.find_by(id:params[:id])
+        options = {
+            include: [:patients]
+        }
+        render json: MedicationSerializer.new(medication, options)
     end
 
     def create
-        @medication = Medication.create(medication_params)
-
-        render json: @medication, status: 200
+        current_patient = Patient.find_by_name(params[:patient])
+        medication = current_patient.medications.create(medication_params)
+        render json: MedicationSerializer.new(medication)
     end
 
-    def update
-        @medication = Medication.find(params[:id])
-        @medication.update(medication_params)
-        
-        render json: @medication, status: 200
-    end
+    # def update
+    #     medication = Medication.find(params[:id])
+    #     medication.update(medication_params)
+    #     render json: MedicationSerializer.new(medication)
+    # end
 
     def destroy
-        @medication = Medication.find(params[:id])
-        @medication.delete
-
-        render json: {medicationId: @medication.id}
+        medication = Medication.find_by(id:params[:id])
+        medication.destroy
+        render json: medication
     end
 
     private
