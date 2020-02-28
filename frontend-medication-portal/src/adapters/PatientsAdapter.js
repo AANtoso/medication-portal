8f2f2ea028a9982e0ec156d9773dbeff5321aa94
AANtoso <adjoaan@gmail.com>
2020-02-27 19:27:15 -0500

    Added post request and fetch.

--- frontend-medication-portal/src/adapters/PatientsAdapter.js
+++ frontend-medication-portal/src/adapters/PatientsAdapter.js
@@ -5,6 +5,23 @@ class PatientsAdapter {
 
     getPatients() {
         return fetch(this.baseUrl)
-        .then(res => res.json())
+            .then(res => res.json())
+    }
+
+    async createPatientDB(params) {
+        let patientData = {
+            name: params[0],
+            mrn: params[1],
+        }
+
+        return fetch(this.baseUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': "application/json"
+                },
+                body: JSON.stringify(patientData)
+            })
+            .then(resp => resp.json())
     }
 }
