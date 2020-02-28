59049b60929f10842d4a842a82171fc24ee4fb13
AANtoso <adjoaan@gmail.com>
2020-02-28 10:48:25 -0500

    Built new constructor.

--- frontend-medication-portal/src/components/patient.js
+++ frontend-medication-portal/src/components/patient.js
@@ -1,17 +1,23 @@
 class Patient {
-    constructor(patient) {
-        this.id = patient.id
-        this.name = patient.name
-        this.mrn = patient.mrn
-        this.medications = patient.medications.map(m => m.name).join(', ')
+    constructor(patientJSON) {
+        this.id = patientJSON.id
+        this.name = patientJSON.name
+        this.mrn = patientJSON.mrn
     }
 
-    patientHTML() {
-        return (`
-            <div id = 'patient-container' class='patient-container card-body border border-secondary' patient-id='${this.id}'>
-                <h4>Patient: ${this.name}</h4></br>
-                <p>MRN: ${this.mrn}</p>
-            </div>
-        `)
-    }
+    // constructor(patient) {
+    //     this.id = patient.id
+    //     this.name = patient.name
+    //     this.mrn = patient.mrn
+    //     this.medications = patient.medications.map(m => m.name).join(', ')
+    // }
+
+    // patientHTML() {
+    //     return (`
+    //         <div id = 'patient-container' class='patient-container card-body border border-secondary' patient-id='${this.id}'>
+    //             <h4>Patient: ${this.name}</h4></br>
+    //             <p>MRN: ${this.mrn}</p>
+    //         </div>
+    //     `)
+    // }
 }
