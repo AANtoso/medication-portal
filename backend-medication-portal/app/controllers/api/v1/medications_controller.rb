851fae096fa512fe3d9046ef62f6957277e84bda
AANtoso <adjoaan@gmail.com>
2020-02-28 09:58:37 -0500

    Removed serializers and edited CRUD functions.

--- backend-medication-portal/app/controllers/api/v1/medications_controller.rb
+++ backend-medication-portal/app/controllers/api/v1/medications_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MedicationsController < ApplicationController
     def index
         medications = Medication.all 
 
-        render json: medications.to_json(include: [:patients])
+        render json: medications
     end
 
     def show
@@ -11,26 +11,28 @@ class Api::V1::MedicationsController < ApplicationController
         options = {
             include: [:patients]
         }
-        render json: MedicationSerializer.new(medication, options)
+        render json: medication
     end
 
     def create
-        current_patient = Patient.find_by_name(params[:patient])
-        medication = current_patient.medications.create(medication_params)
-        render json: MedicationSerializer.new(medication)
+        patient = Patient.find_by(id: params[:patient_id])
+        medication = Medication.create(medication_params)
+        # current_patient = Patient.find_by_name(params[:patient])
+        # medication = current_patient.medications.create(medication_params)
+        render json: medication, include: :patient
     end
 
     # def update
     #     medication = Medication.find(params[:id])
     #     medication.update(medication_params)
-    #     render json: MedicationSerializer.new(medication)
+    #     render json: medication, include: :patient
     # end
 
-    def destroy
-        medication = Medication.find_by(id:params[:id])
-        medication.destroy
-        render json: medication
-    end
+    # def destroy
+    #     medication = Medication.find_by(id:params[:id])
+    #     medication.destroy
+    #     render json: medication.id
+    # end
 
     private
     def medication_params
