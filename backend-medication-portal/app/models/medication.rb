f8ea5b05abb4186419a8670baa3493eb89d12139
AANtoso <adjoaan@gmail.com>
2020-02-28 16:09:45 -0500

    Deleted unnecessary code.

--- backend-medication-portal/app/models/medication.rb
+++ backend-medication-portal/app/models/medication.rb
@@ -1,9 +1,3 @@
 class Medication < ApplicationRecord
     belongs_to :patient
-    
-    # has_many :prescriptions, dependent: :destroy
-    # has_many :patients, through: :prescriptions
-
-    # validates :name, presence: true
-    # validates :name, uniqueness: { case_sensitive: false}
 end
--- backend-medication-portal/app/models/patient.rb
+++ backend-medication-portal/app/models/patient.rb
@@ -1,8 +1,3 @@
 class Patient < ApplicationRecord
     has_many :prescriptions
-    # , dependent: :destroy
-    # has_many :medications, through: :prescriptions
-
-    # validates :name, presence: true
-    # validates :name, uniqueness: { case_sensitive: false}
 end
