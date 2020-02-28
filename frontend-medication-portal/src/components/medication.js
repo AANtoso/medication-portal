76b37aade66f4728be291ace6c25801e496618d2
AANtoso <adjoaan@gmail.com>
2020-02-28 11:41:04 -0500

    Commented out part ofcomponent file. Added some css styling.

--- frontend-medication-portal/src/components/medication.js
+++ frontend-medication-portal/src/components/medication.js
@@ -1,30 +1,30 @@
 class Medication {
     constructor(medication) {
-        this.id = medication.id
-        this.name = medication.name
-        this.class = medication.pharm_class
-        this.indication = medication.indication
-        this.dose = medication.dose
-        this.frequency = medication.frequency
-        this.note = medication.note
-        this.patients = this.getPatientNames(medication.patients)
+        this.id = medicationJSON.id
+        this.name = medicationJSON.name
+        this.class = medicationJSON.pharm_class
+        this.indication = medicationJSON.indication
+        this.dose = medicationJSON.dose
+        this.frequency = medicationJSON.frequency
+        this.note = medicationJSON.note
+            // this.patients = this.getPatientNames(medication.patients)
     }
 
-    getPatientNames(patients) {
-        return patients.map(1 => 1. name).join(',')
-    }
+    //     getPatientNames(patients) {
+    //         return patients.map(1 => 1. name).join(',')
+    //     }
 
-    medicationHTML() {
-        return (`
-            <div id = 'medication-container' class='medication-container card-body border border-secondary' medication-id='${this.id}'>
-                <h4>Medication: ${this.name}</h4></br>
-                <p>Pharm Class: ${this.pharm_class}</p>
-                <p>Indication: ${this.indication}</p>
-                <p>Dose: ${this.dose}</p>
-                <p>Frequency: ${this.frequency}</p>
-                <p>Note: ${this.note}</p>
-                <button name ='delete-medication' class"delete-button btn btn-secondary btn-sm">Delete</button>
-            </div>
-        `)
-    }
+    //     medicationHTML() {
+    //         return (`
+    //             <div id = 'medication-container' class='medication-container card-body border border-secondary' medication-id='${this.id}'>
+    //                 <h4>Medication: ${this.name}</h4></br>
+    //                 <p>Pharm Class: ${this.pharm_class}</p>
+    //                 <p>Indication: ${this.indication}</p>
+    //                 <p>Dose: ${this.dose}</p>
+    //                 <p>Frequency: ${this.frequency}</p>
+    //                 <p>Note: ${this.note}</p>
+    //                 <button name ='delete-medication' class"delete-button btn btn-secondary btn-sm">Delete</button>
+    //             </div>
+    //         `)
+    //     }
 }
